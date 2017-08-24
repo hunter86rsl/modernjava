4ae288f01034b4888be52c9bf147d5d5e841ae67
hunter86rsl <hunter86@onet.eu>
2017-08-24 20:48:36 +0200

    chapter 1

---
+++ Rozdzial1/src/com/apress/bookstore/client/BookApp.java
@@ -0,0 +1,38 @@
+package com.apress.bookstore.client;
+
+import java.util.List;
+
+import com.apress.bookstore.dao.BookDAO;
+import com.apress.bookstore.dao.BookDAOImpl;
+import com.apress.bookstore.model.Book;
+
+public class BookApp {
+	private static BookDAO bookDAO = new BookDAOImpl();
+
+	public static void main(String[] args) {
+		// List all books
+		System.out.println("Lista wszystkich ksi¹¿ek:");
+		findAllBooks();
+
+		System.out.println("++++++++++++++++++++++++++++++");
+
+		System.out.println("Wyszukiwanie ksi¹¿ek za pomoc¹ s³owa kluczowego: Java");
+
+		searchBooks("java");
+
+	}
+
+	private static void findAllBooks() {
+		List<Book> books = bookDAO.findAllBooks();
+		for (Book book : books) {
+			System.out.println(book);
+		}
+	}
+
+	private static void searchBooks(String keyWord) {
+		List<Book> books = bookDAO.searchBooksByKeyword(keyWord);
+		for (Book book : books) {
+			System.out.println(book);
+		}
+	}
+}
